82f2499c3301d9b8648bf48eb86cbe701b1b2785
Andrew Yatsura <andrewyazura203@gmail.com>
2021-06-11 21:51:17 +0300

    Move bot to telegram_bot folder

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -14,9 +14,14 @@ bot = Bot(**config['telegram-bot'])
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
     await message.reply(phrases['start'])
+
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.reply(phrases['help'])
 
 
 if __name__ == '__main__':
