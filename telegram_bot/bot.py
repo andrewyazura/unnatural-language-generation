60a331af4ff3dddd2810e239df35e76e1ebdf277
Andrew Yatsura <andrewyazura203@gmail.com>
2021-06-16 16:20:26 +0300

    Move from aiogram to python-telegram-bot

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,28 +1,36 @@
 import logging
 
 import yaml
-from aiogram import Bot, Dispatcher, executor, types
+from telegram.ext import CommandHandler, Filters, Updater
+from telegram.ext.messagehandler import MessageHandler
 
-with open('config.yml', 'r') as stream:
+with open('telegram_bot/bot_config.yml', 'r') as stream:
     config = yaml.safe_load(stream)
 
-with open('phrases.yml', 'r') as stream:
+with open('telegram_bot/bot_phrases.yml', 'r') as stream:
     phrases = yaml.safe_load(stream)
 
 logging.basicConfig(**config['logging'])
-bot = Bot(**config['telegram-bot'])
-dp = Dispatcher(bot)
+logger = logging.getLogger(__name__)
 
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.reply(phrases['start'])
+def start(update, context):
+    update.message.reply_text(phrases['start'])
 
 
-@dp.message_handler(commands=['help'])
-async def help_command(message: types.Message):
-    await message.reply(phrases['help'])
+def help_command(update, context):
+    update.message.reply_text(phrases['help'])
+
+
+def main():
+    updater = Updater(**config['telegram-bot'])
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', help_command))
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    main()
