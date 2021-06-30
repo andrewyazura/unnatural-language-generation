7ce9ce35acd9a201508086e6049b6f7d75145f7d
Andrew Yatsura <andrewyazura203@gmail.com>
2021-06-30 11:37:22 +0300

    Add user graphs path to bot config

--- telegram_bot/helpers.py
+++ telegram_bot/helpers.py
@@ -1,11 +1,16 @@
 import json
 import os
 
+import yaml
 from networkx.readwrite import node_link_data, node_link_graph
 
 
+with open('telegram_bot/bot_config.yml', 'r') as stream:
+    config = yaml.safe_load(stream)
+
+
 def get_user_graph(user_id):
-    path = f'user_graphs/{user_id}.json'
+    path = os.path.join(config['user-graphs-path'], f'{user_id}.json')
 
     if os.path.exists(path):
         with open(path, 'r') as f:
@@ -13,12 +18,12 @@ def get_user_graph(user_id):
 
 
 def update_user_graph(user_id, graph):
-    path = f'user_graphs/{user_id}.json'
+    path = os.path.join(config['user-graphs-path'], f'{user_id}.json')
 
     with open(path, 'w+') as f:
         json.dump(node_link_data(graph), f)
 
 
 def delete_user_graph(user_id):
-    path = f'user_graphs/{user_id}.json'
+    path = os.path.join(config['user-graphs-path'], f'{user_id}.json')
     os.remove(path)
