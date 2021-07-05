6eb880b2f9d06a29e4123cc1acf5f3791a497578
Andrew Yatsura <andrewyazura203@gmail.com>
2021-07-06 00:21:39 +0300

    Implement random_sequence()

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import random
+
 import tokenize_uk as tn
-from text_generation import tokens_to_graph
+
+from text_generation import join_tokens, random_sequence, tokens_to_graph
 
 with open('tmp/text.txt', 'r') as f:
-    t = f.read()
+    t = f.read().lower()
 
 tokens = tn.tokenize_words(t)
-graph = tokens_to_graph(tokens)
+graph = tokens_to_graph(tokens, 3)
+start = random.choice(
+    [n.split() for n in graph.nodes if graph[n] and len(n.split()) == 3]
+)
+sequence = random_sequence(graph, 200, start, 3)
+output = join_tokens(sequence)
+print(output)
--- text_generation/core.py
+++ text_generation/core.py
@@ -4,19 +4,15 @@ import string
 import networkx as nx
 
 
-def join_tokens(seq):
-    characters = string.punctuation
-    seq = iter(seq)
-    current = next(seq)
+def join_tokens(tokens):
+    result = tokens[0]
 
-    for nxt in seq:
-        if nxt in characters:
-            current += nxt
-        else:
-            yield current
-            current = nxt
+    for token in tokens[1:]:
+        if token not in string.punctuation:
+            result += ' '
+        result += token
 
-    yield current
+    return result
 
 
 def update_graph(graph, key, value):
@@ -32,11 +28,26 @@ def tokens_to_graph(tokens, order=1, graph=None):
 
     for index, token in enumerate(tokens):
         if index >= order:
-            pre = ''.join(join_tokens(tokens[index - order : index]))
+            pre = join_tokens(tokens[index - order : index])
             update_graph(graph, pre, token)
 
     return graph
 
 
-def random_sequence(graph, length, start_token):
-    pass
+def random_sequence(graph, length, start_tokens, order=1):
+    sequence = [*start_tokens]
+
+    for index in range(order, length - order):
+        pre = join_tokens(sequence[index - order : index])
+        next_tokens = graph[pre]
+
+        if not next_tokens:
+            token = '.'
+            sequence += token
+            continue
+
+        weights = [next_tokens[t]['weight'] for t in next_tokens]
+        token = random.choices(list(next_tokens), weights)[0]
+        sequence.append(token)
+
+    return sequence
