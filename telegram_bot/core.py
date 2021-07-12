77b86e8dfa3f4c2cc8783b029c7a1e7a4cab1c0a
Andrew Yatsura <andrewyazura203@gmail.com>
2021-07-12 17:40:40 +0300

    Add /create_graph and /delete_graph

--- telegram_bot/core.py
+++ telegram_bot/core.py
@@ -1,13 +1,13 @@
 import logging
 
+import networkx as nx
+from networkx.readwrite import node_link_data, node_link_graph
 from telegram import (
-    ChatAction,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
     ParseMode,
 )
 from telegram.ext import (
-    CallbackQueryHandler,
     CommandHandler,
     Defaults,
     Filters,
@@ -15,6 +15,7 @@ from telegram.ext import (
     PicklePersistence,
     Updater,
 )
+from telegram.ext.conversationhandler import ConversationHandler
 from text_generation import (
     convert_tokens_to_graph,
     generate_random_sequence,
@@ -29,14 +30,76 @@ phrases = load_yaml('telegram_bot/bot_phrases.yaml')
 logging.basicConfig(**config['logging']['basic-config'])
 logger = logging.getLogger(__name__)
 
+GRAPH_NAME_STATE = 42
+
 
 def start_command(update, context):
+    if not context.user_data:
+        context.user_data['graphs'] = {}
+
     update.message.reply_text(phrases['start'])
     update.message.reply_text(phrases['help'])
 
 
 def help_command(update, context):
     update.message.reply_text(phrases['help'])
+
+
+def cancel_command(update, context):
+    reply_markup = ReplyKeyboardRemove()
+    update.message.reply_text(phrases['cancelled'], reply_markup=reply_markup)
+    return ConversationHandler.END
+
+
+def create_graph_entry(update, context):
+    if len(context.user_data['graphs']) >= 3:
+        update.message.reply_text(phrases['error']['many-graphs'])
+        return ConversationHandler.END
+
+    update.message.reply_text(phrases['ask-graph-name'])
+    return GRAPH_NAME_STATE
+
+
+def create_graph(update, context):
+    if update.message.text in context.user_data['graphs']:
+        update.message.reply_text(phrases['error']['graph-exists'])
+
+    else:
+        graph = nx.DiGraph()
+        context.user_data['graphs'][update.message.text] = node_link_data(
+            graph
+        )
+        update.message.reply_text(phrases['success']['saved'])
+
+    return ConversationHandler.END
+
+
+def delete_graph_entry(update, context):
+    custom_keyboard = [[name] for name in context.user_data['graphs']]
+
+    if not custom_keyboard:
+        update.message.reply_text(phrases['error']['no-graphs'])
+        return ConversationHandler.END
+
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text(
+        phrases['ask-graph-name'], reply_markup=reply_markup
+    )
+    return GRAPH_NAME_STATE
+
+
+def delete_graph(update, context):
+    g = context.user_data['graphs'].pop(update.message.text, None)
+
+    if not g:
+        update.message.reply_text(phrases['error']['no-such-graph'])
+        return GRAPH_NAME_STATE
+
+    reply_markup = ReplyKeyboardRemove()
+    update.message.reply_text(
+        phrases['success']['done'], reply_markup=reply_markup
+    )
+    return ConversationHandler.END
 
 
 def error_handler(update, context):
@@ -61,6 +124,32 @@ def run_bot():
 
     dispatcher.add_handler(CommandHandler('start', start_command))
     dispatcher.add_handler(CommandHandler('help', help_command))
+    dispatcher.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler('delete_graph', delete_graph_entry)],
+            states={
+                GRAPH_NAME_STATE: [
+                    MessageHandler(
+                        Filters.text & (~Filters.command), delete_graph
+                    )
+                ]
+            },
+            fallbacks=[CommandHandler('cancel', cancel_command)],
+        )
+    )
+    dispatcher.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler('create_graph', create_graph_entry)],
+            states={
+                GRAPH_NAME_STATE: [
+                    MessageHandler(
+                        Filters.text & (~Filters.command), create_graph
+                    )
+                ]
+            },
+            fallbacks=[CommandHandler('cancel', cancel_command)],
+        )
+    )
 
     dispatcher.add_error_handler(error_handler)
 
