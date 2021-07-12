33b66c8c9793a7dc37267df359df9fe0667a06a1
Andrew Yatsura <andrewyazura203@gmail.com>
2021-07-12 22:00:19 +0300

    Add graph management

--- telegram_bot/core.py
+++ telegram_bot/core.py
@@ -2,20 +2,17 @@ import logging
 
 import networkx as nx
 from networkx.readwrite import node_link_data, node_link_graph
-from telegram import (
-    ReplyKeyboardMarkup,
-    ReplyKeyboardRemove,
-    ParseMode,
-)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from telegram.ext import (
+    CallbackQueryHandler,
     CommandHandler,
+    ConversationHandler,
     Defaults,
     Filters,
     MessageHandler,
     PicklePersistence,
     Updater,
 )
-from telegram.ext.conversationhandler import ConversationHandler
 from text_generation import (
     convert_tokens_to_graph,
     generate_random_sequence,
@@ -36,6 +33,7 @@ GRAPH_NAME_STATE = 42
 def start_command(update, context):
     if not context.user_data:
         context.user_data['graphs'] = {}
+        context.user_data['current_graph'] = None
 
     update.message.reply_text(phrases['start'])
     update.message.reply_text(phrases['help'])
@@ -46,9 +44,7 @@ def help_command(update, context):
 
 
 def cancel_command(update, context):
-    reply_markup = ReplyKeyboardRemove()
-    update.message.reply_text(phrases['cancelled'], reply_markup=reply_markup)
-    return ConversationHandler.END
+    update.message.reply_text(phrases['cancelled'])
 
 
 def create_graph_entry(update, context):
@@ -74,32 +70,69 @@ def create_graph(update, context):
     return ConversationHandler.END
 
 
-def delete_graph_entry(update, context):
-    custom_keyboard = [[name] for name in context.user_data['graphs']]
+def current_graph(update, context):
+    if not context.user_data['graphs']:
+        update.message.reply_text(phrases['error']['no-graphs'])
+        return
+
+    update.message.reply_text(
+        phrases['current-graph'].format(context.user_data['current_graph'])
+    )
+
+
+def my_graphs(update, context):
+    custom_keyboard = [
+        [InlineKeyboardButton(name, callback_data='use.' + name)]
+        for name in context.user_data['graphs']
+    ]
 
     if not custom_keyboard:
         update.message.reply_text(phrases['error']['no-graphs'])
-        return ConversationHandler.END
+        return
 
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    reply_markup = InlineKeyboardMarkup(custom_keyboard)
     update.message.reply_text(
-        phrases['ask-graph-name'], reply_markup=reply_markup
+        phrases['choose-graph'], reply_markup=reply_markup
     )
-    return GRAPH_NAME_STATE
 
 
 def delete_graph(update, context):
-    g = context.user_data['graphs'].pop(update.message.text, None)
+    custom_keyboard = [
+        [
+            InlineKeyboardButton(name, callback_data='del.' + name)
+            for name in context.user_data['graphs']
+        ]
+    ]
 
-    if not g:
-        update.message.reply_text(phrases['error']['no-such-graph'])
-        return GRAPH_NAME_STATE
+    if not custom_keyboard:
+        update.message.reply_text(phrases['error']['no-graphs'])
+        return
 
-    reply_markup = ReplyKeyboardRemove()
+    reply_markup = InlineKeyboardMarkup(custom_keyboard)
     update.message.reply_text(
-        phrases['success']['done'], reply_markup=reply_markup
+        phrases['choose-graph'], reply_markup=reply_markup
     )
-    return ConversationHandler.END
+
+
+def inline_button_callback(update, context):
+    query = update.callback_query
+    query.answer()
+
+    data = query.data.split('.')
+    other = ''.join(data[1:])
+
+    if data[0] == 'use':
+        context.user_data['current_graph'] = other
+        query.edit_message_text(text=phrases['changed-graph'].format(other))
+
+    elif data[0] == 'del':
+        g = context.user_data['graphs'].pop(other, None)
+
+        if not g:
+            query.edit_message_text(text=phrases['error']['no-such-graph'])
+            return
+
+        query.edit_message_text(text=phrases['deleted-graph'].format(other))
 
 
 def error_handler(update, context):
@@ -124,19 +157,10 @@ def run_bot():
 
     dispatcher.add_handler(CommandHandler('start', start_command))
     dispatcher.add_handler(CommandHandler('help', help_command))
-    dispatcher.add_handler(
-        ConversationHandler(
-            entry_points=[CommandHandler('delete_graph', delete_graph_entry)],
-            states={
-                GRAPH_NAME_STATE: [
-                    MessageHandler(
-                        Filters.text & (~Filters.command), delete_graph
-                    )
-                ]
-            },
-            fallbacks=[CommandHandler('cancel', cancel_command)],
-        )
-    )
+    dispatcher.add_handler(CommandHandler('current_graph', current_graph))
+    dispatcher.add_handler(CommandHandler('my_graphs', my_graphs))
+    dispatcher.add_handler(CommandHandler('delete_graph', delete_graph))
+    dispatcher.add_handler(CallbackQueryHandler(inline_button_callback))
     dispatcher.add_handler(
         ConversationHandler(
             entry_points=[CommandHandler('create_graph', create_graph_entry)],
