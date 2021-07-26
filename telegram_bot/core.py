a4d29928f8faf7d4b574d021d7061e5d04403e6f
Andrew Yatsura <andrewyazura203@gmail.com>
2021-07-26 23:36:02 +0300

    Prepare /upload command

--- telegram_bot/core.py
+++ telegram_bot/core.py
@@ -29,6 +29,7 @@ logging.basicConfig(**config['logging']['basic-config'])
 logger = logging.getLogger(__name__)
 
 GRAPH_NAME_STATE = 42
+UPLOAD_DATA = 43
 
 
 def start_command(update, context):
@@ -44,8 +45,96 @@ def help_command(update, context):
     update.message.reply_text(phrases['help'])
 
 
-def cancel_command(update, context):
-    update.message.reply_text(phrases['cancelled'])
+def new_graph_command(update, context):
+    if len(context.user_data['graphs']) >= 3:
+        update.message.reply_text(phrases['error']['many-graphs'])
+        return ConversationHandler.END
+
+    update.message.reply_text(phrases['ask-graph-name'])
+    return GRAPH_NAME_STATE
+
+
+def new_graph_end(update, context):
+    if update.message.text in context.user_data['graphs']:
+        update.message.reply_text(phrases['error']['graph-exists'])
+
+    else:
+        graph = nx.DiGraph()
+        context.user_data['graphs'][update.message.text] = node_link_data(
+            graph
+        )
+        update.message.reply_text(phrases['success']['saved'])
+
+    return ConversationHandler.END
+
+
+def graphs_command(update, context):
+    curr = context.user_data['current_graph']
+    custom_keyboard = [
+        [InlineKeyboardButton(name, callback_data='use.' + name)]
+        for name in context.user_data['graphs']
+        if name != curr
+    ]
+
+    if curr:
+        custom_keyboard.insert(
+            0,
+            [
+                InlineKeyboardButton(
+                    curr + phrases['current'], callback_data='use.' + curr
+                )
+            ],
+        )
+
+    if not custom_keyboard:
+        update.message.reply_text(phrases['error']['no-graphs'])
+        return
+
+    reply_markup = InlineKeyboardMarkup(custom_keyboard)
+    update.message.reply_text(
+        phrases['choose-graph'], reply_markup=reply_markup
+    )
+
+
+def delete_graph_command(update, context):
+    custom_keyboard = [
+        [
+            InlineKeyboardButton(name, callback_data='del.' + name)
+            for name in context.user_data['graphs']
+        ]
+    ]
+
+    if not custom_keyboard:
+        update.message.reply_text(phrases['error']['no-graphs'])
+        return
+
+    reply_markup = InlineKeyboardMarkup(custom_keyboard)
+    update.message.reply_text(
+        phrases['choose-graph'], reply_markup=reply_markup
+    )
+
+
+def inline_button_callback(update, context):
+    query = update.callback_quesry
+    query.answer()
+
+    data = query.data.split('.')
+    other = ''.join(data[1:])
+
+    if data[0] == 'use':
+        context.user_data['current_graph'] = other
+        query.edit_message_text(text=phrases['changed-graph'].format(other))
+
+    elif data[0] == 'del':
+        g = context.user_data['graphs'].pop(other, None)
+        if context.user_data['current_graph'] == other:
+            context.user_data['current_graph'] = None
+
+        if not g:
+            query.edit_message_text(text=phrases['error']['no-such-graph'])
+            return
+
+        query.edit_message_text(text=phrases['deleted-graph'].format(other))
 
 
 def generate_command(update, context):
@@ -54,7 +143,7 @@ def generate_command(update, context):
     )
 
     if not graph_data:
-        return
+        return  # TODO: user has to choose a graph
 
     try:
         if context.matches:
@@ -86,92 +175,26 @@ def generate_command(update, context):
     update.message.reply_text(output)
 
 
-def create_graph_entry(update, context):
-    if len(context.user_data['graphs']) >= 3:
-        update.message.reply_text(phrases['error']['many-graphs'])
-        return ConversationHandler.END
-
-    update.message.reply_text(phrases['ask-graph-name'])
-    return GRAPH_NAME_STATE
-
-
-def create_graph(update, context):
-    if update.message.text in context.user_data['graphs']:
-        update.message.reply_text(phrases['error']['graph-exists'])
-
-    else:
-        graph = nx.DiGraph()
-        context.user_data['graphs'][update.message.text] = node_link_data(
-            graph
-        )
-        update.message.reply_text(phrases['success']['saved'])
-
-    return ConversationHandler.END
-
-
-def current_graph(update, context):
+def upload_command(update, context):
     if not context.user_data['graphs']:
-        update.message.reply_text(phrases['error']['no-graphs'])
         return
 
-    update.message.reply_text(
-        phrases['current-graph'].format(context.user_data['current_graph'])
-    )
-
-
-def list_graphs(update, context):
-    custom_keyboard = [
-        [InlineKeyboardButton(name, callback_data='use.' + name)]
-        for name in context.user_data['graphs']
-    ]
-
-    if not custom_keyboard:
-        update.message.reply_text(phrases['error']['no-graphs'])
+    elif not context.user_data['current_graph']:
         return
 
-    reply_markup = InlineKeyboardMarkup(custom_keyboard)
-    update.message.reply_text(
-        phrases['choose-graph'], reply_markup=reply_markup
-    )
+    return UPLOAD_DATA
 
 
-def delete_graph(update, context):
-    custom_keyboard = [
-        [
-            InlineKeyboardButton(name, callback_data='del.' + name)
-            for name in context.user_data['graphs']
-        ]
-    ]
-
-    if not custom_keyboard:
-        update.message.reply_text(phrases['error']['no-graphs'])
-        return
-
-    reply_markup = InlineKeyboardMarkup(custom_keyboard)
-    update.message.reply_text(
-        phrases['choose-graph'], reply_markup=reply_markup
-    )
+def receive_file(update, context):
+    pass
 
 
-def inline_button_callback(update, context):
-    query = update.callback_query
-    query.answer()
+def receive_text(update, context):
+    pass
 
-    data = query.data.split('.')
-    other = ''.join(data[1:])
 
-    if data[0] == 'use':
-        context.user_data['current_graph'] = other
-        query.edit_message_text(text=phrases['changed-graph'].format(other))
-
-    elif data[0] == 'del':
-        g = context.user_data['graphs'].pop(other, None)
-
-        if not g:
-            query.edit_message_text(text=phrases['error']['no-such-graph'])
-            return
-
-        query.edit_message_text(text=phrases['deleted-graph'].format(other))
+def cancel_command(update, context):
+    update.message.reply_text(phrases['cancelled'])
 
 
 def error_handler(update, context):
@@ -196,25 +219,38 @@ def run_bot():
 
     dispatcher.add_handler(CommandHandler('start', start_command))
     dispatcher.add_handler(CommandHandler('help', help_command))
-    dispatcher.add_handler(CommandHandler('generate', generate_command))
-    dispatcher.add_handler(
-        MessageHandler(Filters.regex(r'^\+(\d+)$'), generate_command)
-    )
-    dispatcher.add_handler(CommandHandler('current_graph', current_graph))
-    dispatcher.add_handler(CommandHandler('list_graphs', list_graphs))
-    dispatcher.add_handler(CommandHandler('delete_graph', delete_graph))
+    dispatcher.add_handler(CommandHandler('graphs', graphs_command))
+    dispatcher.add_handler(CommandHandler('deletegraph', delete_graph_command))
     dispatcher.add_handler(CallbackQueryHandler(inline_button_callback))
     dispatcher.add_handler(
         ConversationHandler(
-            entry_points=[CommandHandler('create_graph', create_graph_entry)],
+            entry_points=[CommandHandler('newgraph', new_graph_command)],
             states={
                 GRAPH_NAME_STATE: [
                     MessageHandler(
-                        Filters.text & (~Filters.command), create_graph
+                        Filters.text & (~Filters.command), new_graph_end
                     )
                 ]
             },
             fallbacks=[CommandHandler('cancel', cancel_command)],
+        )
+    )
+    dispatcher.add_handler(CommandHandler('generate', generate_command))
+    dispatcher.add_handler(
+        MessageHandler(Filters.regex(r'^\+(\d+)$'), generate_command)
+    )
+    dispatcher.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler('upload', upload_command)],
+            states={
+                UPLOAD_DATA: [
+                    MessageHandler(Filters.document, receive_file),
+                    MessageHandler(
+                        Filters.text & (~Filters.command), receive_text
+                    ),
+                ]
+            },
+            fallback=[CommandHandler('cancel', cancel_command)],
         )
     )
 
