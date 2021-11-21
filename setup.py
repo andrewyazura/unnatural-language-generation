c4bbe9589fe1aceb3c9b3d0854316f466b94d41d
Andrew Yatsura <andrewyazura203@gmail.com>
2021-11-21 23:56:20 +0200

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="unnatural-language-generation",
     version="3.0.0",
     packages=find_packages(),
+    install_requires=["click", "networkx", "tokenize-uk"],
     include_package_data=True,
     entry_points={"console_scripts": ["ulg = text_generator.scripts.cli:cli"]},
 )
